1398ecc5f9a4b05c6f820007be5a8e6f9d6e13dc
Tolya Fam <famovkin@yandex.ru>
2021-12-06 05:56:10 +0300

    feat: сверстать бургер меню

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import CloseIcon from "@mui/icons-material/Close";
 
 function Header() {
+  const [burgerState, setBurgerState] = useState(false);
+
   return (
     <Container>
       <Logo>
@@ -16,10 +19,78 @@ function Header() {
         <a href="#">Solar Panels</a>
       </Menu>
       <RightMenu>
-        <a href="$">Shop</a>
-        <a href="$">Account</a>
-        <a href="$">Menu</a>
+        <a href="#">Shop</a>
+        <a href="#">Account</a>
+        <CustomMenu onClick={() => setBurgerState(true)}>
+          <a href="#">Menu</a>
+        </CustomMenu>
       </RightMenu>
+
+      <BurgerMenu show={burgerState}>
+        <CloseWrapper>
+          <CustomClose onClick={() => setBurgerState(false)} />
+        </CloseWrapper>
+        <li>
+          <a href="#">Model S</a>
+        </li>
+        <li>
+          <a href="#">Model 3</a>
+        </li>
+        <li>
+          <a href="#">Model X</a>
+        </li>
+        <li>
+          <a href="#">Model Y</a>
+        </li>
+        <li>
+          <a href="#">Solar Roof</a>
+        </li>
+        <li>
+          <a href="#">Solar Panels</a>
+        </li>
+        <li>
+          <a href="#">Existing Inventory</a>
+        </li>
+        <li>
+          <a href="#">Used Inventory</a>
+        </li>
+        <li>
+          <a href="#">Trade-in</a>
+        </li>
+        <li>
+          <a href="#">Test Drive</a>
+        </li>
+        <li>
+          <a href="#">Powerwall</a>
+        </li>
+        <li>
+          <a href="#">Commertial Energy</a>
+        </li>
+        <li>
+          <a href="#">Utilities</a>
+        </li>
+        <li>
+          <a href="#">Charging</a>
+        </li>
+        <li>
+          <a href="#">Find Us</a>
+        </li>
+        <li>
+          <a href="#">Support</a>
+        </li>
+        <li>
+          <a href="#">Investor Relations</a>
+        </li>
+        <li>
+          <a href="#">Shop</a>
+        </li>
+        <li>
+          <a href="#">Account</a>
+        </li>
+        <li>
+          <a href="#">More</a>
+        </li>
+      </BurgerMenu>
     </Container>
   );
 }
@@ -36,6 +107,7 @@ const Container = styled.div`
   top: 0;
   left: 0;
   right: 0;
+  z-index: 1;
 `;
 
 const Logo = styled.div`
@@ -65,4 +137,45 @@ const RightMenu = styled.div`
   font-weight: 600;
   flex-wrap: nowrap;
   gap: 30px;
+`;
+
+const CustomMenu = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 600;
+  flex-wrap: nowrap;
+`;
+
+const BurgerMenu = styled.div`
+  overflow-y: scroll;
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  background-color: #fff;
+  width: 270px;
+  z-index: 3;
+  list-style: none;
+  text-align: left;
+  padding: 30px 30px;
+  transform: ${(props) => (props.show ? "translateX(0)" : "translateX(100%)")};
+  transition: transform 0.35s ease-in;
+  li {
+    padding: 8px;
+  }
+  a {
+    font-weight: 600;
+  }
+`;
+
+const CustomClose = styled(CloseIcon)`
+  color: red;
+  cursor: pointer;
+`;
+
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  margin-bottom: 30px;
 `;
